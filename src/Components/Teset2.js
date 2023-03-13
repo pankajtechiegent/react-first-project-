f8260f57571bbc5f181fc10e271880dfeb4dbe17
pankaj <pankaj.agrawal@techiegent.com>
2023-03-14 02:25:49 +0530

    react first project

---
+++ src/Components/Teset2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Teset2 = () => {
+  return (
+    <div style={{color:"red"}}><h1>third project </h1></div>
+  )
+}
+
+export default Teset2
---
+++ src/Components/Test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Teset2 from './Teset2'
+
+const Test = () => {
+  return (
+    <div style={{color:"blue"}}><h1>second react project</h1>
+    <Teset2/>
+    </div>
+    
+  )
+}
+
+export default Test
